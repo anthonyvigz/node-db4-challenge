8100032ea3e8c6350a0e26af485abbfb6ce5743e
anthonyvigz <anthonyvigz@gmail.com>
2019-08-29 19:12:27 -0400

    set up ingredients recipes table in migrations

--- data/migrations/20190829190603_create_recipes.js
+++ data/migrations/20190829190603_create_recipes.js
@@ -8,6 +8,18 @@ exports.up = function(knex) {
     tbl.increments('id');
     tbl.string('ingredient_name', 128).notNullable();
     })
+    .createTable('ingredients_recipes', tbl => {
+        tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id').inTable('recipes');
+        tbl.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id').inTable('ingredients');
+        tbl.float('quantity').notNullable();
+        tbl.primary(['recipe_id', 'ingredient_id'])
+    })
   )
 };
 
